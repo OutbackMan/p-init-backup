e943fb61c384229203f352836d07c35d97fb3891
Ryan <re.mcclue@gmail.com>
2018-04-13 13:23:49 +1000

    Finalised sdl-base

--- c/files/sdl-base.c
+++ c/files/sdl-base.c
@@ -17,11 +17,11 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-INTERNAL const unsigned SUCCESS = 1
-INTERNAL const unsigned FAILURE = 0
-INTERNAL const unsigned WINDOW_FAILURE_CODE = 1
-INTERNAL const unsigned RENDERER_FAILURE_CODE = 2
-INTERNAL const unsigned TEXTURE_FAILURE_CODE = 3
+INTERNAL const unsigned SUCCESS = 1;
+INTERNAL const unsigned FAILURE = 0;
+INTERNAL const unsigned WINDOW_FAILURE_CODE = 1;
+INTERNAL const unsigned RENDERER_FAILURE_CODE = 2;
+INTERNAL const unsigned TEXTURE_FAILURE_CODE = 3;
 
 INTERNAL void game_quit(const int game_exit_code)
 {
@@ -29,67 +29,44 @@ INTERNAL void game_quit(const int game_exit_code)
 	exit(game_exit_code);
 }
 
-INTERNAL void texture_set_colour(SDL_Texture* texture, SDL_Color* colour)
+INTERNAL void game__modify_texture(SDL_Texture* game_texture, SDL_Color* colour)
 {
-	int texture_width = 0;
-	int texture_height = 0;
-	SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
+	int game_texture_width = 0;
+	int game_texture_height = 0;
+	SDL_QueryTexture(game_texture, NULL, NULL, &game_texture_width, &game_texture_height);
 
-	void* texture_pixels = NULL;	
-	int texture_pitch = 0;
+	void* game_texture_pixels = NULL;	
+	int game_texture_pitch = 0;
 
-	uint32_t* texture_pixel = NULL;
+	u32* game_texture_pixel = NULL;
 
-	if (SDL_LockTexture(texture, NULL, &texture_pitch, &texture_width) < 0) {
-		fprintf(stderr, "Error encountered: %s\n", SDL_GetError());	
-		return;
+	if (SDL_LockTexture(game_texture, NULL, &game_texture_pixels, &game_texture_pitch) < 0) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to lock game texture: %s\n", SDL_GetError());	
+		game_quit(TEXTURE_FAILURE_CODE);
 	}
 
-	for (int row = 0; row < texture_width; ++row) {
-		texture_pixel = (uint32_t *)((uint8_t *)(texture_pixels) + row * texture_pitch)
-		for (int col = 0; col < texture_height; ++col) {
-			*texture_pixel++ = (0xFF000000 | (colour->r << 16) | (colour->g << 8) | (colour->b));
+	for (int game_texture_row = 0; game_texture_row < game_texture_width; ++game_texture_row) {
+		game_texture_pixel = (u32 *)((u8 *)(game_texture_pixels) + game_texture_row * game_texture_pitch);
+		for (int game_texture_col = 0; game_texture_col < game_texture_height; ++game_texture_col) {
+			*game_texture_pixel++ = (0xFF000000 | (colour->r << 16) | (colour->g << 8) | (colour->b));
 		}
 	}	
 	
-	SDL_UnlockTexture(texture);	
+	SDL_UnlockTexture(game_texture);	
 }
 
-INTERNAL void resize_sdl_texture(SDL_Texture* texture, SDL_Renderer* renderer, int width, int height)
+INTERNAL void game__resize_texture(SDL_Texture* game_texture, SDL_Renderer* game_renderer, int current_game_width, int current_game_height)
 {
-	if (texture != NULL) {
-		SDL_DestroyTexture(texture);	
-	}	
+	SDL_DestroyTexture(game_texture);	
 
-	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
-					width, height);
-}
+	game_texture = SDL_CreateTexture(game_renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
+					current_game_width, current_game_height);
 
-INTERNAL void game__draw_texture(SDL_Window* game_window, SDL_Texture* game_texture)
-{
-	int current_game_width = 0;
-	int current_game_height = 0;
-
-	SDL_GetWindowSize(game_window, &window_width, &window_height);
-
-	SDL_Renderer* renderer = SDL_GetRenderer(window);
-
-	void* texture_pixels = NULL;
-	int texture_pitch = 0; // bytes per row
-	if (SDL_LockTexture(window_texture, NULL, &texture_pitch, &texture_width) < 0) {
-		fprintf(stderr, "Error encountered: %s\n", SDL_GetError());	
-		return;
+	if (game_texture == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Unable to resize game texture: %s\n", SDL_GetError());	
+		game_quit(TEXTURE_FAILURE_CODE);
 	}
-
-	// Obtain desired colour from somewhere
-	memset(texture_pixels, 0, texture_pitch * window_height);
-
-	SDL_UnlockTexture(window_texture);
-
-	SDL_RenderCopy(renderer, window_texture, NULL, NULL); 
-	SDL_RenderPresent(renderer);
 }
-
 
 INTERNAL int game_start(const unsigned game_width, const unsigned game_height)
 {
@@ -115,7 +92,7 @@ INTERNAL int game_start(const unsigned game_width, const unsigned game_height)
 		game_quit(RENDERER_FAILURE_CODE);
 	}
 
-	SDL_Texture* game_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
+	SDL_Texture* game_texture = SDL_CreateTexture(game_renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
 											game_width, game_height);
 
 	if (game_texture == NULL) {
@@ -128,7 +105,7 @@ INTERNAL int game_start(const unsigned game_width, const unsigned game_height)
 	while (game_is_running) {
 		SDL_Event event = {0};
 		while (SDL_PollEvent(&event)) {
-			switch (event->type) {
+			switch (event.type) {
 			 case SDL_QUIT:
 			 {
 				game_is_running = false;	 
@@ -136,53 +113,29 @@ INTERNAL int game_start(const unsigned game_width, const unsigned game_height)
 
 			 case SDL_WINDOWEVENT:
 			 {
-				switch (event->window.event) {
+				switch (event.window.event) {
 				 case SDL_WINDOWEVENT_RESIZED:
 				 {
-					game__resize_texture(game_texture, event->window.data1, event->window.data2);
+					game__resize_texture(game_texture, game_renderer, event.window.data1, event.window.data2);
 				 } break;
 				 case SDL_WINDOWEVENT_EXPOSED:
 				 {
-					game__draw_texture(game_texture);
+					SDL_RenderCopy(game_renderer, game_texture, NULL, NULL); 
+					SDL_RenderPresent(game_renderer);
 				 } break;
 				}
 			 } break;
 
 			 case SDL_KEYDOWN:
-			 case SDL_KEYUP:
 			 {
-				SDL_Keycode entered_keycode = event->key.keysym.sym;
-				 
-			 }
-		bool key_is_down = (event->key.state == SDL_PRESSED);
-		bool key_was_down = false;
-		if (event->key.state == SDL_RELEASED || event->key.repeat != 0) {
-			key_was_down = true;		
-		}
-
-		if (event->key.repeat == 0) {
-			if (keycode == SDLK_w) {
-				printf("w %s\n", (key_is_down ? "is down" : "was down"));
-			} else if (keycode == SDLK_a) {
-				
-			}
-		}
-
-		bool alt_key_was_down = (event->key.keysym.mod & KMOD_ALT);
-		if (keycode == SDLK_F4 && alt_key_was_down) {
-			// ...		
-		}
-	 }
-
-	 default:
-	 {
-		break;
-	 }
-	}		
-					
-		}	
-		// logic, draw
-	}
+				SDL_Keycode entered_keycode = event.key.keysym.sym;
+				if (entered_keycode == SDLK_w) {
+					puts("w");		
+				}
+			 } break;
+			} // end-event-switch	
+		} // end-event-while 
+	} // end-game-while	
 
 	// cleanup
 
