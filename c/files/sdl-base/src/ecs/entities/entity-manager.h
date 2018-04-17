467f14f8e77104384d3476d966e6012deadcbd56
Ryan <re.mcclue@gmail.com>
2018-04-17 12:03:51 +1000

    common defs

--- c/files/sdl-base/src/ecs/entities/entity-manager.h
+++ c/files/sdl-base/src/ecs/entities/entity-manager.h
@@ -1,10 +1,48 @@
+typedef unsigned int entity_index_t;
+
 typedef struct {
-	bitarray* component_mask;
+	size_t num_components;
+	size_t cap_components;
+
+	BitArray* component_mask;
 
 	DisplacementComponent* displacement_component;
 } EntityManager;
 
-unsigned create_entity(EntityManager* entity_manager)
+status_code_t entity_manager__create_component_mask(BitArray* component_mask, const size_t num_components)
+{
+	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
+		bit_array_create(&component_mask[component_index]), starting_component_capacity);
+	}
+			
+}
+
+status_code_t entity_manager_create(EntityManager* entity_manager, const size_t starting_component_capacity)
+{
+	entity_manager->component_mask = calloc(sizeof(BitArray), starting_component_capacity);
+
+	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
+		bit_array_create(&(entity_manager->component_mask[component_index]), starting_component_capacity);
+	}
+
+	entity_manager->num_components = 0;
+	entity_manager->cap_components = starting_component_capacity;
+
+	entity_manager->displacement_component = calloc(sizeof(DisplacementComponent), starting_component_capacity); 
+}
+
+void entity_manager_destroy(EntityManager* entity_manager)
+{
+	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
+		bit_array_free(&(entity_manager->component_mask[component_index]), starting_component_capacity);
+	}
+	
+	bit_array_free(entity_manager->component_mask);	
+
+	free(entity_manager->displacement_component);
+}
+
+entity_index_t entity_manager_create_entity(EntityManager* entity_manager)
 {
 	unsigned entity = 0;
 
