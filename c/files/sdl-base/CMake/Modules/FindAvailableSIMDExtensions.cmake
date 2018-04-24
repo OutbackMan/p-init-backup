0613305bcf384acce66285bfcf4186b66202ae90
Ryan <re.mcclue@gmail.com>
2018-04-24 10:30:03 +1000

    more dim

--- c/files/sdl-base/CMake/Modules/FindAvailableSIMDExtensions.cmake
+++ c/files/sdl-base/CMake/Modules/FindAvailableSIMDExtensions.cmake
@@ -7,18 +7,44 @@ set(__find_available_simd_extensions_cmake__ TRUE)
 # assume all cpus support sse2 (introduced 2001).
 # sse3 (introduced 2004)
 # ssse3 (2006)
+
+
 # https://software.intel.com/en-us/articles/intel-architecture-and-processor-identification-with-cpuid-model-and-family-numbers
+#  CPUID Signature Values of Of Recent Intel Microarchitectures 
+# (https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-optimization-manual.pdf)
+
 # look at wiki pages for sse, sse2, etc. to find what architecture first supported
-function(_parse_simd_extensions cpu_info)
+function(_parse_simd_extensions cpu_vendor_str cpu_arch_family_num cpu_arch_model_num)
 	set(_available_simd_extensions PARENT_SCOPE)
+	list(APPEND _available_simd_extensions sse)
+	list(APPEND _available_simd_extensions sse2)
+	list(APPEND _available_simd_extensions sse3)
 	
-	if(cpu_vendor STREQUAL "GenuineIntel")
-		# pentium pro <---> skylake
-		if(cpu_family STREQUAL "6")
-			if(cpu_model EQUAL 0x3a)
-			
-			
-		
+	if(cpu_vendor_str STREQUAL "GenuineIntel")
+		if(cpu_arch_family_num STREQUAL "6")
+			# Skylake
+			if(cpu_arch_model_num STREQUAL "58")
+			# Broadwell
+			if(cpu_arch_model_num STREQUAL "58")
+			# Haswell
+			if(cpu_arch_model_num STREQUAL "58")
+			# IvyBridge
+			if(cpu_arch_model_num STREQUAL "58")
+
+				list(APPEND _available_simd_extensions sse3)
+			# SandyBridge
+			elseif(cpu_arch_model_num STREQUAL "42" OR cpu_arch_model_num STREQUAL "46")
+
+			# Westmere 
+			elseif(cpu_arch_model_num STREQUAL "37" OR cpu_arch_model_num STREQUAL "44" OR cpu_arch_model_num STREQUAL "47")
+
+			# Nehalem
+			elseif(cpu_arch_model_num STREQUAL "37" OR cpu_arch_model_num STREQUAL "44" OR cpu_arch_model_num STREQUAL "47")
+
+			# Enhanced Core
+			elseif(cpu_arch_model_num STREQUAL "37" OR cpu_arch_model_num STREQUAL "44" OR cpu_arch_model_num STREQUAL "47")
+
+
 		endif(cpu_family STREQUAL "6")
 		
 		# pentium
@@ -32,97 +58,56 @@ function(_parse_simd_extensions cpu_info)
 		endif(cpu_family STREQUAL "15")
 	
 	
-	endif(cpu_vendor STREQUAL "GenuineIntel")
+	elseif(cpu_vendor_str STREQUAL "AuthenticAMD")
+		
 	
-	if(cpu_vendor STREQUAL "AuthenticAMD")
+	else(cpu_vendor_str STREQUAL "GenuineIntel")
+		message(WARNING "CPU vendor is not either of the supported Intel or AMD vendors. Defaulting to 'sse', 'sse2' and 'sse3' extensions.")
+	endif(cpu_vendor_str STREQUAL "GenuineIntel")
 	
-	
-
-	string(REGEX MATCH "^.*(sse).*$" "\\1" _sse_there cpu_info)
-	list(APPEND _available_simd_extensions _sse_there)
-
-	string(REGEX MATCH "^.*(sse2).*$" "\\1" _sse2_there cpu_info)
-	list(APPEND _available_simd_extensions _sse2_there)
 
 	# SSE3 intel code name Prescott New Instructions
-	string(REGEX MATCH "^.*(sse3|pni).*$" "\\1" _sse3_there cpu_info)
-	list(APPEND _available_simd_extensions _sse3_there)
-
-	string(REGEX MATCH "^.*(ssse3).*$" "\\1" _ssse3_there cpu_info)
-	list(APPEND _available_simd_extensions _ssse3_there)
-
-	# mac will have sse4.1
-	string(REGEX MATCH "^.*(sse4_1).*$" "\\1" _sse4_1_there cpu_info)
-	list(APPEND _available_simd_extensions _sse4_1_there)
-
-	string(REGEX MATCH "^.*(sse4_2).*$" "\\1" _sse4_2_there cpu_info)
-	list(APPEND _available_simd_extensions _sse4_2_there)
-
-	string(REGEX MATCH "^.*(avx).*$" "\\1" _avx_there cpu_info)
-	list(APPEND _available_simd_extensions _avx_there)
-
-	string(REGEX MATCH "^.*(avx2).*$" "\\1" _avx2_there cpu_info)
-	list(APPEND _available_simd_extensions _avx2_there)
-
-	string(REGEX MATCH "^.*(avx512).*$" "\\1" _avx512_there cpu_info)
-	list(APPEND _available_simd_extensions _avx512_there)
-
 endfunction(_parse_simd_extensions)
-	
-# CPUID Signature Values of Of Recent Intel Microarchitectures
-# skylake: 4E 5E -->
 
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+
 	file(READ "/proc/cpuinfo" _cpu_info)
-	string(REGEX MATCH ".*vendor_id[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_vendor _cpu_info)
-	string(REGEX MATCH ".*cpu family[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_architecture_family_id _cpu_info)
-	string(REGEX MATCH ".*model[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_model _cpu_info)
-	string(REGEX MATCH ".*flags[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_flags _cpu_info)
+	string(REGEX MATCH ".*vendor_id[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_vendor_str _cpu_info)
+	string(REGEX MATCH ".*cpu family[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_arch_family_num _cpu_info)
+	string(REGEX MATCH ".*model[\t]*:[\t]+([a-zA-Z0-9_-]+).*" "\\1" _cpu_arch_model_num _cpu_info)
 
-	if(_cpu_vendor STREQUAL "GenuineIntel")
+elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
 
-	endif()
+	execute_process(COMMAND "sysctl -n machdep.cpu.vendor" OUTPUT_VARIABLE _cpu_vendor_str)
+    execute_process(COMMAND "sysctl -n machdep.cpu.family" OUTPUT_VARIABLE _cpu_arch_family_num)
+    execute_process(COMMAND "sysctl -n machdep.cpu.model"  OUTPUT_VARIABLE _cpu_arch_model_num)
 
-	_parse_simd_extensions(_cpuinfo)
+elseif(CMAKE_SYSTEM_NAME MATCHES "Windows")
+
+	execute_process(COMMAND "REG QUERY HKLM\\Hardware\\Description\\System\\CentralProcessor\\0 /v VendorIdentifier"
+		OUTPUT_VARIABLE _cpu_vendor_str)
+	execute_process(COMMAND "REG QUERY HKLM\\Hardware\\Description\\System\\CentralProcessor\\0 /v Identifier"
+		OUTPUT_VARIABLE _cpu_id)
+	string(REGEX MATCH ".* Family ([0-9]+).*" "\\1" _cpu_arch_family_num _cpu_id)
+	string(REGEX MATCH ".* Model ([0-9]+).*" "\\1" _cpu_arch_model_num _cpu_id)
+
+else(CMAKE_SYSTEM_NAME MATCHES "Linux")
+
+	message(WARNING "OS is not either of the supported OS's Linux, Mac or Windows")
 
 endif(CMAKE_SYSTEM_NAME MATCHES "Linux")
 
+_parse_simd_extensions(_cpu_vendor_str _cpu_arch_family_num _cpu_arch_model_num)
 
-if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-	execute_process(COMMAND "sysctl -a | grep machdep.cpu.features" OUTPUT_VARIABLE _cpuinfo)
+foreach(simd_extension ${_available_simd_extensions})
+	CheckCCompilerFlag(simd_extension supports_simd_extension)
+	if(supports_simd_extension)
+		message(WARNING "Compiler doesn't support simd extension: '${simd_extension}'")
+	else(supports_simd_extension)
+		# add compiler flag
+	endif(supports_simd_extension)
 
-	_parse_simd_extensions(_cpuinfo)
-
-endif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-
-
-# refer to Intel SDM (software developers manual. this also contains ISA)
-
-if(CMAKE_SYSTEM_NAME MATCHES "Windows")
-	execute_process(COMMAND "REG QUERY HKLM\\Hardware\\Description\\System\\CentralProcessor\\0 /v VendorIdentifier"
-		OUTPUT_VARIABLE _cpu_vendor)
-	execute_process(COMMAND "HKLM\\Hardware\\Description\\System\\CentralProcessor\\0 /v Identifier"
-		OUTPUT_VARIABLE _cpu_id)
-
-# assume sse and sse2 support
-
-# CPUID signature values of architectures
-# product comparison for instruction set extensions
-
-# vendor_id (intel, amd)
-# model (intel core i5-7200u @ 2.50GHz)
-# family (determines architecture, code, 4000, 4th generation, Haswell)
-# flags
-
-# check for all others
-
-
-
-endif(CMAKE_SYSTEM_NAME MATCHES "Windows")
-
-
-
-
+endforeach()
 
 
 
