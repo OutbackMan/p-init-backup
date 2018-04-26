d82914d9b8e69853e9080b55ac96bd79993da847
Ryan <re.mcclue@gmail.com>
2018-04-26 15:45:10 +1000

    data

--- c/files/sdl-base/src/ecs/entities/entity-manager.h
+++ c/files/sdl-base/src/ecs/entities/entity-manager.h
@@ -12,34 +12,38 @@ typedef struct {
 status_code_t entity_manager__create_component_mask(BitArray* component_mask, const size_t num_components)
 {
 	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
-		bit_array_create(&component_mask[component_index]), starting_component_capacity);
+		bit_array_create(&component_mask[component_index], starting_component_capacity);
 	}
 			
 }
 
 status_code_t entity_manager_create(EntityManager* entity_manager, const size_t starting_component_capacity)
 {
-	entity_manager->component_mask = calloc(sizeof(BitArray), starting_component_capacity);
+	BitArray bit_array_temp = GAME_DEFAULT_INITIALISER;
 
 	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
-		bit_array_create(&(entity_manager->component_mask[component_index]), starting_component_capacity);
+		if (bit_array_create(&bit_array_temp, starting_component_capacity) != SUCCESS) {
+			GAME_LEAVE(FAILURE);		
+		}
+
+		GAME_STRETCHY_BUF_PUSH(entity_manager->component_mask, bit_array_temp);
+
+		GAME_STRETCHY_BUF_PUSH(entity_manager->displacement_component, GAME_DEFAULT_INITIALISER); 
 	}
 
 	entity_manager->num_components = 0;
 	entity_manager->cap_components = starting_component_capacity;
 
-	entity_manager->displacement_component = calloc(sizeof(DisplacementComponent), starting_component_capacity); 
 }
 
 void entity_manager_destroy(EntityManager* entity_manager)
 {
-	for (size_t component_index = 0; component_index < starting_component_capacity; ++component_index) {
+	for (size_t component_index = 0; component_index < STRETCHY_BUF_LEN(entity_manager->component_mask); ++component_index) {
 		bit_array_free(&(entity_manager->component_mask[component_index]), starting_component_capacity);
 	}
-	
-	bit_array_free(entity_manager->component_mask);	
 
-	free(entity_manager->displacement_component);
+	GAME_STRETCHY_BUF_FREE(entity_manager->component_mask);
+	GAME_STRETCHY_BUF_FREE(entity_manager->displacement);
 }
 
 entity_index_t entity_manager_create_entity(EntityManager* entity_manager)
