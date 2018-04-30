105e561a5680ec0493860e935f6dc9dc1352ca59
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-05-01 08:45:04 +1000

    Update tetromino.c

--- c/files/sdl-base/src/assets/tetromino.c
+++ c/files/sdl-base/src/assets/tetromino.c
@@ -1,18 +1,31 @@
 // typedef SDL_Colour colour_asset;
+#include "sdl-colour-wrappers.h"
 
 SDL_Color tetromino[7][16] = {0};
 
-GAME_STATUS game_assets__load_tetromino(game_asset* tetromino)
+enum {
+	TETROMINO_LEFT,
+	TETROMINO_RIGHT
+};
+
+colour_asset* tetrominoes = NULL;
+if (colours_assets__load_tetrominoes(tetrominoes)) {
+	tetrominoes[TETROMINO_LEFT][TOP_LEFT];
+}
+
+GAME_STATUS game_assets__load_tetromino(colour_asset* tetromino)
 {
 	game_asset left_tetromino[16] = {
 		RED, 		
 	}
+	// anything with BUF_PUSH requires __leave:
 	BUF_PUSH(tetromino, left_tetromino);
+	
 }
 
-void game_assets__free(GAME_AssetManager* asset_manager)
+void game_assets__free(colour_asset* asset_manager)
 {
-		free(asset_manager);	
+		BUF_FREE(asset_manager);	
 }
 
 tetromino[0] = {
