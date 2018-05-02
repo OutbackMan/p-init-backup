03528d6fc81c6a947f6723a5d38323c000c574dd
Russell Coight <32256825+OutbackMan@users.noreply.github.com>
2018-05-03 08:31:03 +1000

    Update sprite.c

--- c/files/sdl-base/src/core/sprite.c
+++ c/files/sdl-base/src/core/sprite.c
@@ -1,21 +1,39 @@
 // Take base size of 'character' as 8 * 8 pixels
 
 typedef struct {
-  size_t width;
-  size_t height;
+  u32 width;
+  u32 height;
   GameCharacter* characters;
 } GameSprite;
 
+// bmp loader
+
+
+
+
+
+
+// ASSUME ALL BLOCK CHARACTERS
+// width, height
+// character_colour
 int load_sprite(GameSprite* game_sprite, const char* sprite_file_name)
 {
   FILE* sprite_file = fopen(sprite_file_name, "rb");
   if (sprite_file != NULL) {
-  
+    u32 game_sprite_width = GAME_DEFAULT_INITIALISER;
+    u32 game_sprite_height = GAME_DEFAULT_INITIALISER;
+    
+    fread(&game_sprite_width, sizeof(game_sprite_width), 1, sprite_file);
+    fread(&game_sprite_height, sizeof(game_sprite_height), 1, sprite_file);
+    
+    game_sprite->characters = xmalloc(sizeof(GameCharacter) * width * height);
+    
+   
   }
   
     
     
-  game_sprite->characters = xmalloc(sizeof(GameCharacter) * width * height);
+  
   
 }
 
