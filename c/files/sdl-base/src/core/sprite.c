0007b2c67145c056ace74bc3849306c5608ceffe
Ryan <re.mcclue@gmail.com>
2018-05-03 10:31:26 +1000

    s

--- c/files/sdl-base/src/core/sprite.c
+++ c/files/sdl-base/src/core/sprite.c
@@ -1,43 +1,96 @@
-// Take base size of 'character' as 8 * 8 pixels
-
 typedef struct {
   u32 width;
   u32 height;
   GameCharacter* characters;
 } GameSprite;
 
-// bmp loader
-
-
-
-
-
-
-// ASSUME ALL BLOCK CHARACTERS
-// width, height
-// character_colour
-int load_sprite(GameSprite* game_sprite, const char* sprite_file_name)
+int load_sprite(GameSprite* game_sprite, FILE* sprite_file)
 {
-  FILE* sprite_file = fopen(sprite_file_name, "rb");
-  if (sprite_file != NULL) {
-    u32 game_sprite_width = GAME_DEFAULT_INITIALISER;
-    u32 game_sprite_height = GAME_DEFAULT_INITIALISER;
-    
-    fread(&game_sprite_width, sizeof(game_sprite_width), 1, sprite_file);
-    fread(&game_sprite_height, sizeof(game_sprite_height), 1, sprite_file);
-    
-    game_sprite->characters = xmalloc(sizeof(GameCharacter) * width * height);
-    
-   
-  }
-  
-    
-    
-  
-  
+	if (sprite_file != NULL) {
+		static const size_t buf_size = 256;
+		char buf[buf_size] = GAME_DEFAULT_INITIALISER;
+		char* buf_handler = NULL;
+
+		buf_handler = fgets(buf, sizeof(buf), sprite_file);
+		if (buf_handler == NULL) {
+			GAME_LOG_FATAL("Unable to read PPM file magic number");
+			return FILE_FAILURE;		
+		}
+		if (strncmp(buf, "P6\n", 3) != 0) {
+			GAME_LOG_FATAL("Non-raw PPM file format given for sprite conversion");
+			return FILE_FAILURE;		
+		}
+
+		do {
+			buf_handler = fgets(buf, sizeof(buf), sprite_file);		
+			if (buf_handler == NULL) {
+				GAME_LOG_FATAL("Unable to read PPM file comment section");
+				return FILE_FAILURE;	
+			}
+		} while (strncmp(buf, "#", 1) == 0);
+		
+		int scan_count = GAME_DEFAULT_INITIALISER;
+
+		scan_count = sscanf(buf, "%u %u", &game_sprite->width, &game_sprite->height);	
+		if (scan_count < 2) {
+			GAME_LOG_FATAL("Positive ASCII decimal values for PPM file width and height not found");
+			return FILE_FAILURE;	
+		}
+
+		unsigned max_colour_value = GAME_DEFAULT_INITIALISER;
+		scan_count = fscanf(sprite_file, "%u", &max_colour_value);
+		if (scan_count < 1) {
+			GAME_LOG_FATAL("Unable to read max colour value for PPM file");
+			return FILE_FAILURE;	
+		}
+		if (max_colour_value != 255) {
+			GAME_LOG_FATAL("Non RGB colour format detected for PPM file");
+			return FILE_FAILURE;	
+		}
+		fseek(sprite_file, 1, SEEK_CUR);
+		
+		GAME_XMALLOC(sprite_file->characters, sizeof(GameCharacter) * sprite_file->width * sprite_file->height);
+
+		u8 red_colour_component = GAME_DEFAULT_INITIALISER;
+		u8 green_colour_component = GAME_DEFAULT_INITIALISER;
+		u8 blue_colour_component = GAME_DEFAULT_INITIALISER;
+		SDL_Colour character_colour = GAME_DEFAULT_INITIALISER;
+
+		for (size_t pixel_row = 0; pixel_row < width; ++pixel_row) {
+			for (size_t pixel_col = 0; pixel_col < height; ++pixel_col) {
+				fread(&red_colour_component, sizeof(u8), 1, sprite_file);
+				fread(&green_colour_component, sizeof(u8), 1, sprite_file);
+				fread(&blue_colour_component, sizeof(u8), 1, sprite_file);
+
+				character_colour = {
+					.r = red_colour_component,
+					.g = green_colour_component,
+					.b = blue_colour_component,
+					.a = 255
+				};
+
+				create_block_character(sprite_file->characters++, &character_colour); 
+			}
+		}
+
+	return SUCCESS;
 }
 
-void destroy_sprite(GameSprite* game_sprite)
+void free_sprite(GameSprite* game_sprite)
 {
   free(game_sprite->characters);
+}
+
+int create_block_character(GameCharacter* character, SDL_Colour* character_colour)
+{
+	character->width = 8;
+	character->height = 8;
+	
+	character->palette->solid_colour = character_colour;
+	character->palette->empty_colour = character_colour;
+
+	character->glyph = {
+			
+	};
+
 }
